58c88c4e32cbc1ba8874012f1f7209593f3bef90
HenriqqG <henriquegagno2@gmail.com>
2025-10-21 19:18:54 -0300

    inicio popup

---
+++ popup/popup.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", async () => {
+  const twitterIcon = document.getElementById("twitterIcon");
+  const watchITLogo = document.getElementById("watchITLogo");
+  twitterIcon.addEventListener("click", () => {
+    chrome.tabs.create({ url: "https://x.com/GagnoHenriqq"});
+  });
+  watchITLogo.addEventListener("click", () => {
+    chrome.tabs.create({ url: "https://watchit-cs.netlify.app/"});
+  });
+});
